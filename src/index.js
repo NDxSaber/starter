4b6f30cc24802a2cc576d27bee5a600805f5e6b2
ENDYSANTOSO-DEV\User <endy.santoso@tiket.com>
2017-08-09 14:24:04 +0700

    add redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
+//--React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//--Redux
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+//--React Router
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import './assets/css/global.css';
-import App from './components/home/App';
-import About from './components/about/About';
+
+//--ServiceWorker
 import registerServiceWorker from './registerServiceWorker';
 
+//--Global Style
+import './assets/css/global.css';
+
+//--Pages
+import App from './components/App'
+// import App from './components/home/App';
+import About from './components/about/About';
+
+let store = createStore(todoApp);
+
 ReactDOM.render(
-	<Router>
-		<div>
-			<Route exact path="/" component={App} />
-			<Route path="/about" component={About} />
-		</div>
-	</Router>, 
+	<Provider store={store}>
+		<Router>
+			<div>
+				<Route exact path="/" component={App} />
+				<Route path="/about" component={About} />
+			</div>
+		</Router>
+	</Provider>,
 	document.getElementById('root')
 );
 registerServiceWorker();
+
+			// <Route path="/about" component={About} />
